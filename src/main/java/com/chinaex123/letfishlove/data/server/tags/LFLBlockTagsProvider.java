0064e5c162accea2d7453285b502e0c04a15ef7a
WitherRedstone <1751216077@qq.com>
2026-06-25 09:40:03 +0800

    修复一个导致的所有shovel相关tag失效的问题

--- src/main/java/com/chinaex123/letfishlove/data/server/tags/LFLBlockTagsProvider.java
+++ src/main/java/com/chinaex123/letfishlove/data/server/tags/LFLBlockTagsProvider.java
@@ -22,7 +22,12 @@ public class LFLBlockTagsProvider extends BlockTagsProvider {
 
     @Override
     public void addTags(HolderLookup.@NotNull Provider pProvider) {
-        RoeBlock.getAllBlocks().forEach(block -> tag(BlockTags.MINEABLE_WITH_SHOVEL).add(block));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).add(
+            LFLBlocks.COD_ROE_BLOCK.get(),
+            LFLBlocks.PUFFERFISH_ROE_BLOCK.get(),
+            LFLBlocks.SALMON_ROE_BLOCK.get(),
+            LFLBlocks.TROPICAL_FISH_ROE_BLOCK.get()
+        );
 
         tag(LFLBlockTags.COD).add(LFLBlocks.COD_ROE_BLOCK.get());
         tag(LFLBlockTags.SALMON).add(LFLBlocks.SALMON_ROE_BLOCK.get());
@@ -131,6 +136,108 @@ public class LFLBlockTagsProvider extends BlockTagsProvider {
         tag(LFLBlockTags.DUTCHMAN_SOCK).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "dutchman_sock_roe_block"));
         tag(LFLBlockTags.ELYTROUT).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "elytrout_roe_block"));
         tag(LFLBlockTags.MANTYVERN).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "mantyvern_roe_block"));
-    }
 
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "tuna_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "mackerel_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "snook_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "angelfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "mahi_mahi_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "sailfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "swordfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "manta_ray_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "aquathorn_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "rainbow_trout_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "brook_trout_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "largemouth_bass_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "smallmouth_bass_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "white_crappie_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "black_crappie_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "yellow_perch_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "carp_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "pike_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "guppy_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "bluegill_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "catfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "walleye_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "arapaima_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "mirage_catfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "sand_tiger_shark_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "slimy_salmon_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "frostbite_flounder_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "sturgeon_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "blossom_bass_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "spore_stalker_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "mooneye_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "bull_shark_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "ocean_perch_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "red_snapper_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "flounder_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "anchovy_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "devils_hole_pupfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "midas_fish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "incandescent_larva_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "bedrock_bug_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "sleepy_carp_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "blue_neonfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "judgment_fish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "deep_blue_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "nephrosilu_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "neptune_koi_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "pluto_snail_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "sun_emblem_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "saturn_cuttlefish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "marstilus_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "uranias_pisces_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "great_white_shark_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "shooting_starfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "coelacanth_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "cave_eel_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "cave_crawler_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "deep_grouper_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "shadow_snapper_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "glowfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "anglerfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "abyss_angler_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "iron_tetra_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "lapis_lanternfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "crystal_shrimp_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "dripstone_darter_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "luminescent_jellyfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "crystalline_carp_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "gilded_minnow_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "bedrock_tetra_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "chasm_eel_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "echo_snapper_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "windbass_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "snatcher_squid_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "voidseeker_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "dragon_fish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "vengeance_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "pentapus_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "darkness_eater_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "shadow_shark_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "alpha_fish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "magma_mackerel_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "ember_koi_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "ash_perch_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "obsidian_pike_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "volcano_tuna_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "inferno_guppy_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "warped_guppy_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "crimson_fangjaw_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "soulscale_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "witherfin_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "blazing_swordfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "pale_clubfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "amber_rockfish_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "enderfin_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "chorus_cod_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "ender_glider_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "endergazer_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "violet_carp_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "red_40_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "dutchman_sock_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "elytrout_roe_block"));
+        tag(BlockTags.MINEABLE_WITH_SHOVEL).addOptional(ResourceLocation.fromNamespaceAndPath("letfishlove", "mantyvern_roe_block"));
+    }
 }
